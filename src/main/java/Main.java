b333e023172d2bf1ddd513b1e1bc9f497705a210
Moisés Luis <benjamin_luis_1999@hotmail.com>
2021-08-18 12:52:13 -0600

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+    private static final String USER = "root2";
+    private static final String PASSWORD = "313346631";
+    private static String URL_MYSQL = "jdbc:mysql://localhost:3306/mi_muebleria";
+
+    public static void main(String[] args) {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL_MYSQL,USER,PASSWORD);
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("INSERT INTO proveedor VALUES (2,'proveedor2', 155548)");
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+}
